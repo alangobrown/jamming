8e8d4a2a7e4fa08b772f98e8cdec21d460cbdca4
Alan Brown <abrown@mac02508.home>
2018-12-06 12:19:38 +0000

    added config

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+var config = {
+  local: {
+    clientID: '7cbcfd7b9e214e8bb8cc559ceb299d8a',
+    redirectURL: 'http://localhost:3000/',
+    corsAnywhere: 'https://cors-anywhere.herokuapp.com/'
+  },
+  surge:{
+    clientID: '1a5d87e7955c4c1aaacdd6bd4b95c5ba',
+    redirectURL: 'http://agb-jamming.surge.sh/',
+    corsAnywhere: 'https://cors-anywhere.herokuapp.com/'
+  }
+};
+
+  module.exports = config;
